a05e86a523b66d2008fc0ff09f94dcd35c0e0a30
Arslan Baig <arn3870@gmail.com>
2023-06-27 23:52:17 +0500

    corrected the no api response error

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
-const accessKey = "w3RGJQfTW-VV23J6QE4tAPcWEtEWmZGpKe0xNdDGdD4";
+document.addEventListener("DOMContentLoaded", () => {
+  const accessKey = "w3RGJQfTW-VV23J6QE4tAPcWEtEWmZGpKe0xNdDGdD4";
 
-const formEl = document.querySelector("form");
-const inputEl = document.getElementById("search-input");
-const searchResults = document.querySelector(".search-results");
-const showMore = document.getElementById("show-more-button");
+  const formEl = document.querySelector("form");
+  const inputEl = document.getElementById("search-input");
+  const searchResults = document.querySelector(".search-results");
+  const showMore = document.getElementById("show-more-button");
 
-let inputData = "";
-let page = 1;
+  let inputData = "";
+  let page = 1;
 
-async function searchImages() {
-  inputData = inputEl.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+  async function searchImages() {
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-  const response = await fetch(url);
-  const data = await response.json();
+    const response = await fetch(url);
+    const data = await response.json();
 
-  const results = data.results;
+    const results = data.results;
 
-  if (page === 1) {
-    searchResults.innerHTML = "";
+    if (page === 1) {
+      searchResults.innerHTML = "";
+    }
+    results.map((result) => {
+      const imageWrapper = document.createElement("div");
+      imageWrapper.classList.add("search-result");
+      const image = document.createElement("img");
+      image.src = result.urls.small;
+      const imageLink = document.createElement("a");
+      imageLink.href = result.links.html;
+      imageLink.target = "blank";
+      imageLink.textContent = result.alt_description;
+
+      imageWrapper.appendChild(image);
+      imageWrapper.appendChild(imageLink);
+      searchResults.appendChild(imageWrapper);
+    });
+
+    page++;
+    if (page > 1) {
+      showMore.style.display = "block";
+    }
   }
-  results.map((result) => {
-    const imageWrapper = document.createElement("div");
-    imageWrapper.classList.add("search result");
-    const image = document.createElement("img");
-    image.src = result.urls.small;
-    const imageLink = document.createElement("a");
-    imageLink.href = result.links.html;
-    imageLink.target = "blank";
-    imageLink.textContent = result.alt_description;
 
-    imageWrapper.appendChild(image);
-    imageWrapper.appendChild(imageLink);
-    searchResults.appendChild(imageWrapper);
+  formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
   });
 
-  page++;
-  if (page > 1) {
-    showMore.style.display = "block";
-  }
-}
-formEl.addEventListener("submit", (event) => {
-  event.preventDefault();
-  page = 1;
-  searchImages();
-});
-
-showMore.addEventListener("click", () => {
-  event.preventDefault();
-  page = 1;
-  searchImages();
+  showMore.addEventListener("click", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
+  });
 });
